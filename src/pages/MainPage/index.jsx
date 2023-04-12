8fdd7c0cde767f9dee5d33d090d90feec8567792
My Name <borisenko_07@mail.ru>
2023-04-12 17:40:10 +0200

    part of a main

--- src/pages/MainPage/index.jsx
+++ src/pages/MainPage/index.jsx
@@ -1,7 +1,16 @@
-import React from 'react'
+import React from 'react';
+import s from './style.module.css';
+import Background from './media/background.jpg';
 
 export default function MainPage() {
-  return (
-    <div>MainPage</div>
-  )
+	return (
+		<div>
+      <div className={s.welcome_wrapper}>
+        <img src={Background} alt="photo" />
+				<h1>Sale</h1>
+				<h2>New season</h2>
+				<button>Sale</button>
+			</div>
+		</div>
+	);
 }
