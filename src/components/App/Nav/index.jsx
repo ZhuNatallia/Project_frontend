d11d1e83281562e719a8d9808ed59609947a6fdf
My Name <borisenko_07@mail.ru>
2023-04-12 16:45:06 +0200

    icons to nav

--- src/components/App/Nav/index.jsx
+++ src/components/App/Nav/index.jsx
@@ -1,27 +1,32 @@
 import React from 'react';
 import s from './style.module.css';
 import { NavLink } from 'react-router-dom';
+import Logo from './media/logo.png';
+import Basket from './media/basket.png';
 
 export default function Nav() {
 	const links = [
-		{ id: 1, label: 'Catalog', to: '/catalog'},
+		{ id: 1, label: 'Catalog', to: '/catalog' },
 		{ id: 2, label: 'Main Page', to: '/' },
-		{ id: 3, label: 'All products', to: '/products/all'},
-		{ id: 4, label: 'All sales', to: '/products/sale'},
-		{ id: 5, label: 'basket', to: '/basket'},
+		{ id: 3, label: 'All products', to: '/products/all' },
+		{ id: 4, label: 'All sales', to: '/products/sale' },
+		/* { id: 5, label: 'basket', to: '/basket'}, */
 	];
 
 	const isActive = ({ isActive }) => (isActive ? s.active : '');
 
 	return (
 		<nav className={s.nav}>
-            {
-                links.map(({id, label, to}) => 
-                    <NavLink className={isActive} key={id} to={to}>
-                        {label}
-                    </NavLink>
-                )
-            }
+			<img src={Logo} alt='logo' />
+			{/* <button>Catalog</button> */}
+			{links.map(({ id, label, to }) => (
+				<NavLink className={isActive} key={id} to={to}>
+					{label}
+				</NavLink>
+			))}
+			<NavLink to='/basket'>
+				<img src={Basket} alt='icons' />
+			</NavLink>
 		</nav>
 	);
 }
