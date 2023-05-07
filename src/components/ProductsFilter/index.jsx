b01370fe72f53602e98ab68b423124de0b4c5ce5
My Name <borisenko_07@mail.ru>
2023-05-07 16:56:18 +0200

    filter have bag

--- src/components/ProductsFilter/index.jsx
+++ src/components/ProductsFilter/index.jsx
@@ -1,57 +1,80 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import s from './style.module.css';
-import { useDispatch } from 'react-redux';
-import { productSortAction } from '../../store/reducer/productsReducer';
+import { useDispatch, useSelector } from 'react-redux';
+import {productFilterAction, productSortAction} from '../../store/reducer/productsReducer';
 
 export default function ProductsFilter() {
+	const list = useSelector((state) => state.products.list);
+
 	const dispatch = useDispatch();
+
 	const sorthOnChange = (event) => {
 		dispatch(productSortAction(+event.target.value));
 	};
-
-	const [state, setState] = useState(false);
-	const [priceFrom, setPriceFrom] = useState("");
-	const [priceTo, setPriceTo] = useState("");
-
-	/* const formHandleFrom = (e) => {
-		if (e.key === 'Enter') {
-			setPriceFrom(e.target.value);
-		}
+	const filterOnChange = (event) => {
+		dispatch(productFilterAction());
 	};
-	const formHandleTo = (e) => {
-		if (e.key === 'Enter') {
-			setPriceTo(e.target.value);
-		}
+	/* const [state, setState] = useState(false); */
+
+	const [priceFilter, setPriceFilter] = useState({
+		min: -Infinity,
+		max: Infinity,
+	});
+	const [products, setProduct] = useState(list);
+
+	/* const filterPrice = () => {
+		setProduct((previosValue) =>
+			previosValue.filter(({show_flg})=> show_flg).map((product) => {
+				const { max, min } = priceFilter;
+				product.show_flg =
+					product.price >= min && product.price <= max;
+				return product;
+			})
+		);
 	}; */
+
+	useEffect(() => {
+		productFilterAction()
+	},[priceFilter])
+
+	const maxInput = (event) => {
+		setPriceFilter((previosValue) => ({
+			...previosValue,
+			max: +event.target.value || Infinity,
+		}));
+	};
+	const minInput = (event) => {
+		setPriceFilter((previosValue) => ({
+			...previosValue,
+			min: +event.target.value || Infinity,
+		}));
+	}; 
 
 	return (
 		<fieldset className={s.container}>
-			<div>
+			<form>
 				<span>Price</span>
 				<input
-					onChange={(e) => setPriceFrom(e.target.value)}
-					/* onKeyDown={formHandleFrom} */
-					value={priceFrom}
 					type='number'
-					placeholder='from'
+					placeholder='min'
+					value={priceFilter.min}
+					onChange={minInput}
 				/>
 				<input
-					onChange={(e) => setPriceTo(e.target.value)}
-					/* onKeyDown={formHandleTo} */
-					name='priceTo'
-					value={priceTo}
 					type='number'
-					placeholder='to'
+					placeholder='max'
+					value={priceFilter.max}
+					onChange={maxInput}
 				/>
-			</div>
-			<div>
+			</form>
+			{/* <div>
 				<p>Discounted items {state ? 'yes' : 'No'}</p>
 				<input
 					type='checkbox'
 					checked={state}
 					onChange={() => setState(!state)}
 				/>
-			</div>
+			</div> */}
 
 			<div>
 				<label htmlFor='sort'>Sorted</label>
--- src/store/reducer/productsReducer.js
+++ src/store/reducer/productsReducer.js
@@ -1,5 +1,6 @@
 const PRODUCTS_LOAD = 'PRODUCTS_LOAD';
 const PRODUCTS_SORT_PRICE = 'PRODUCTS_SORT_PRICE';
+const PRODUCTS_FILTER_PRICE = 'PRODUCTS_FILTER_PRICE';
 
 export const productLoadAction = (payload) => ({
 	type: PRODUCTS_LOAD,
@@ -9,6 +10,10 @@ export const productSortAction = (payload) => ({
 	type: PRODUCTS_SORT_PRICE,
 	payload,
 });
+export const productFilterAction = (payload) => ({
+	type: PRODUCTS_FILTER_PRICE,
+	payload,
+});
 
 const getPrice = ({ filteredPrice }) => +filteredPrice;
 
@@ -16,10 +21,24 @@ export const productsReducer = (state = [], action) => {
 	if (action.type === PRODUCTS_LOAD) {
 		return action.payload.map((item) => ({
 			...item,
+			show_flg: true,
 			filteredPrice: item.discont_price ? item.discont_price : item.price,
 		}));
+	} else if (action.type === PRODUCTS_FILTER_PRICE) {
+		const filterPrice = () => {
+			/* setProduct((previosValue) =>
+				previosValue
+					.filter(({ show_flg }) => show_flg)
+					.map((product) => {
+						const { max, min } = priceFilter;
+						product.show_flg = product.price >= min && product.price <= max;
+						return product;
+					})
+			); */
+		}; return filterPrice
+		
 	} else if (action.type === PRODUCTS_SORT_PRICE) {
-		return [...state].sort((a, b) => {
+		return [...state]/* .slice() */.sort((a, b) => {
 			if (action.payload === 1) {
 				return getPrice(a) - getPrice(b);
 			} else if (action.payload === 2) {
