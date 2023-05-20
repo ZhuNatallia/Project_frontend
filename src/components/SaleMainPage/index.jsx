2682c4db2884afc704439bf3a5bf5cb6952ef0a0
My Name <borisenko_07@mail.ru>
2023-05-20 22:30:00 +0200

    add POST SaleMainPage

--- src/components/SaleMainPage/index.jsx
+++ src/components/SaleMainPage/index.jsx
@@ -3,12 +3,33 @@ import s from './style.module.css';
 import BackgroundPhoto from './media/5.jpg';
 
 export default function SaleMainPage() {
+	const urlSale = 'http://localhost:3333/sale/send';
+	const onSubmit = async (event) => {
+		event.preventDefault();
+		try {
+			const response = await fetch(urlSale, {
+				method: 'POST',
+				body: JSON.stringify({
+					id: Date.now(),
+					phone: +event.target.phone.value,
+				}),
+			});
+			const data = await response.json();
+			console.log(data);
+		} catch (error) {
+			console.log(error);
+		}
+		event.target.phone.value = '';
+	};
+
 	return (
 		<div className={s.container}>
 			<h4>5 % off </h4>
 			<p>on the first order</p>
-			<input type='tel' name='' placeholder='+49' />
-			<button className={s.btn}>Get a discount</button>
+			<form onSubmit={onSubmit}>
+				<input type='tel' name='phone' placeholder='+49' />
+				<button className={s.btn}>Get a discount</button>
+			</form>
 			<img className={s.backgroundPhoto} src={BackgroundPhoto} alt='photo' />
 		</div>
 	);
