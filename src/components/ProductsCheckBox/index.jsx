d5a2cb8c2170b22ee7ecbc1f4263128d07287e85
My Name <borisenko_07@mail.ru>
2023-05-25 19:08:17 +0200

    add hiding checkbox

--- src/components/ProductsCheckBox/index.jsx
+++ src/components/ProductsCheckBox/index.jsx
@@ -2,18 +2,26 @@ import React from 'react';
 import { productSale } from '../../store/slice/productSlice';
 import { useDispatch } from 'react-redux';
 import s from './style.module.css';
+import { useLocation } from 'react-router-dom';
 
 export default function ProductsCheckBox() {
 	const dispatch = useDispatch();
+	const saleLocation = useLocation();
 
 	const productSaleOnChange = (event) => {
 		dispatch(productSale(event.target.checked));
 	};
 
 	return (
-		<div className={s.container}>
-			<label>Discounted items:</label>
-			<input type='checkbox' onChange={productSaleOnChange} />
+		<div>
+			{saleLocation.pathname === '/products/sale' ? (
+				''
+			) : (
+				<div className={s.container}>
+					<label>Discounted items:</label>
+					<input type='checkbox' onChange={productSaleOnChange} />
+				</div>
+			)}
 		</div>
 	);
 }
